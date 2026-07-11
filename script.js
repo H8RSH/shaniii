b3effd4451f04059236e0bad5bf62954d888c93e
H8RSH <143129476+H8RSH@users.noreply.github.com>
2026-07-11 14:48:09 +0530

    Add button click event to redirect after delay

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const btn=document.getElementById("giftBtn");
 
+btn.onclick=function(){
+
+btn.innerHTML="❤️ Opening...";
+
+setTimeout(function(){
+
+window.location.href="flower.html";
+
+},1800);
+
+}
